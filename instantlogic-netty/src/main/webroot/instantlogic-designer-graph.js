541a199c4f210d6befe7442e5ac53c4d10996b16
Johan Gorter <johan@johangorter.com>
2014-03-10 22:20:29 +0100

    presence working again

--- instantlogic-netty/src/main/webroot/instantlogic-designer-graph.js
+++ instantlogic-netty/src/main/webroot/instantlogic-designer-graph.js
@@ -1,27 +1,76 @@
 YUI.add('instantlogic-designer-graph', function(Y) {
 
   var test = {
-    "type": "Graph",
-    "id": "0",
-    "startNodeId": "1",
-    "nodes": [
-      {
-        "id": "1",
-        "text": "Organisatie"
-      },
-      {
-        "id": "2",
-        "text": "Adres"
-      }
-    ],
-    "edges": {
-      "normal": [{
-        "from": "1",
-        "to": "2",
-        "type": "normal"
-      }]
-    }
-  };	
+		  "type": "Graph",
+		  "id": "graph1",
+		  "nodes": [
+		   {
+		    "id": "EntityDesign_comment",
+		    "text": "comment"
+		   },
+		   {
+		    "id": "EntityDesign_issue",
+		    "text": "issue"
+		   },
+		   {
+		    "id": "EntityDesign_issueStatus",
+		    "text": "issueStatus"
+		   },
+		   {
+		    "id": "EntityDesign_project",
+		    "text": "project"
+		   },
+		   {
+		    "id": "EntityDesign_user",
+		    "text": "user"
+		   }
+		  ],
+		  "edges": {
+		   "inherits": [],
+		   "ownsMany": [
+		    {
+		     "id": "RelationDesign_comments",
+		     "from": "EntityDesign_issue",
+		     "to": "EntityDesign_comment"
+		    },
+		    {
+		     "id": "RelationDesign_issues",
+		     "from": "EntityDesign_project",
+		     "to": "EntityDesign_issue"
+		    },
+		    {
+		     "id": "RelationDesign_users",
+		     "from": "EntityDesign_project",
+		     "to": "EntityDesign_user"
+		    }
+		   ],
+		   "ownsOne": [],
+		   "one": [
+		    {
+		     "id": "RelationDesign_by",
+		     "from": "EntityDesign_comment",
+		     "to": "EntityDesign_user"
+		    },
+		    {
+		     "id": "RelationDesign_assignee",
+		     "from": "EntityDesign_issue",
+		     "to": "EntityDesign_user"
+		    },
+		    {
+		     "id": "RelationDesign_reporter",
+		     "from": "EntityDesign_issue",
+		     "to": "EntityDesign_user"
+		    },
+		    {
+		     "id": "RelationDesign_status",
+		     "from": "EntityDesign_issue",
+		     "to": "EntityDesign_issueStatus"
+		    }
+		   ],
+		   "many": []
+		  },
+		  "startNodeId": "EntityDesign_project"
+		 };	
 	
   var ns = Y.namespace('instantlogic.designer.graph');
   var html = Y.html;
